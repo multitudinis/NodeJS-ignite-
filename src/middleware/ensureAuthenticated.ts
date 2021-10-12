20acf72b55160b146d582915b32ee3abc63796f7
multitudinis <lucaspessoa.ds@outlook.com>
2021-10-11 21:00:11 -0300

    sunny day

---
+++ src/middleware/ensureAuthenticated.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+    sub: string
+}
+
+export async function ensureAuthenticated(
+    request: Request,
+    response: Response,
+    next: NextFunction
+){
+    const authHeader = request.headers.authorization
+
+    if(!authHeader){
+        throw new Error("Token missing!")
+    }
+
+    const [,token] = authHeader.split(" ")
+    try{
+        const {sub: user_id} = verify(token, "bba4b91d2fe360c9c15f54e5815a66d5") as IPayload
+        const usersRepository = new UsersRepository()
+        const user = await usersRepository.findById(user_id)
+
+        if(!user){
+            throw new Error("User does not exists")
+        }
+        next()
+    } catch {
+        throw new Error("Invalid token!")
+     }
+}
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import { ensureAuthenticated } from "../middleware/ensureAuthenticated";
 import { createSpecificationController } from "../modules/cars/useCases/createSpecification/createSpecificationController";
 
 
 const specificationsRoutes = Router()
 const CreateSpecificationController = new createSpecificationController()
 
+specificationsRoutes.use(ensureAuthenticated)
 specificationsRoutes.post("/", CreateSpecificationController.handle)
 
 export {specificationsRoutes}
